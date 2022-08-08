7ef1447be6519dbcfe93c775cd6276a431a074ec
vaeu <oneum@pm.me>
2022-08-08 08:21:23 +0100

    print two columns of input and output

--- cmd/fibonacci/main.go
+++ cmd/fibonacci/main.go
@@ -17,7 +17,8 @@ func main() {
 	max := flag.Int("max", MAX, "last number to iterate through")
 	flag.Parse()
 
+	fmt.Printf("INPUT\tOUTPUT\n\n")
 	for i := *min; i <= *max; i++ {
-		fmt.Println(fibonacci.Calculate(i))
+		fmt.Printf("%d\t%d\n", i, fibonacci.Calculate(i))
 	}
 }
